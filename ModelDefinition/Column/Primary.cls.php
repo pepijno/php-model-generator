edc13b4e7f4352c153a5077a35223bbdd224bd81
Unknown <pepijn.overbeeke@gmail.com>
2018-02-24 16:03:48 +0100

    Implemented Primary decorator

---
+++ ModelDefinition/Column/Primary.cls.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ModelDefinition\Column;
+
+use ModelDefinition\Column\ColumnInterface;
+
+final class Primary implements ColumnInterface
+{
+    private $_column;
+
+    public function __construct(ColumnInterface $column)
+    {
+        $this->_column = $column;
+    }
+
+    public function name() : string
+    {
+        return $this->_column->name();
+    }
+}
