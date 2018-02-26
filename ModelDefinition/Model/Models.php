f74bc3f24d5170bdafac9361bcc92854e086ffbe
Unknown <pepijn.overbeeke@gmail.com>
2018-02-26 22:17:39 +0100

    Added namespace, directory and submodels to models class

--- ModelDefinition/Model/Models.php
+++ ModelDefinition/Model/Models.php
@@ -7,9 +7,25 @@ use ModelDefinition\Model\Model;
 final class Models
 {
     private $_models = [];
+    private $_subModels = [];
+    private $_namespace;
+    private $_directory;
+
+    public function __construct($namespace, $dir)
+    {
+        $this->_namespace = $namespace;
+        $this->_directory = $directory;
+    }
+
+    public function addSubmodels(Models $models)
+    {
+        $this->_subModels[] = $models;
+        return $this;
+    }
 
     public function addModel(Model $model)
     {
         $this->_models[] = $model;
+        return $this;
     }
 }
